a9eabc80fdb5fb5d5d653d4a15d21c995ac793f4
Rowayda <rowaidasayad@gmail.com>
2021-08-11 01:56:34 +0300

    first commit

---
+++ app/src/main/java/com/example/texttospeechudemy/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.texttospeechudemy;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button readBtn = findViewById(R.id.readBtn);
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            if (requestCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //means if TTS is available
+                tts = new TextToSpeech(this, this);
+                tts.setLanguage(Locale.US);
+            } else {
+                //tts not installed so we install it
+                Intent intent = new Intent();
+                intent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(intent);
+            }
+        }
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            //flush to empty
+            tts.speak("Hi Hazem", TextToSpeech.QUEUE_FLUSH, null);
+        } else if (status == TextToSpeech.ERROR) {
+            Toast.makeText(MainActivity.this, "TTS failed", Toast.LENGTH_SHORT).show();
+            tts.shutdown();
+        }
+    }
+}
